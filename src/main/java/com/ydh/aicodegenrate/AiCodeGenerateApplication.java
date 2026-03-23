a8374e061c579992f825e93162af1b51b4fc507c
ydh_ni <2539696925@qq.com>
2026-03-23 22:10:45 +0800

    后端项目初始化，首次提交

---
+++ src/main/java/com/ydh/aicodegenrate/AiCodeGenerateApplication.java
@@ -0,0 +1,14 @@
+package com.ydh.aicodegenrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class AiCodeGenerateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeGenerateApplication.class, args);
+    }
+
+}
